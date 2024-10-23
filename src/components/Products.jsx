17a6267801de1d6a5e865b24688a5e150abab7cf
Aditya Verma <138475873+AdityaExplorer@users.noreply.github.com>
2024-10-23 19:39:35 +0530

    'adding_images_product_section

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -38,14 +38,14 @@ const Products = () => {
             </div>
             <div className="carousel-item">
               <img
-                src="https://media.istockphoto.com/id/1400194993/photo/cappuccino-art.webp?a=1&b=1&s=612x612&w=0&k=20&c=CN_BYkO9GKOH4JgMebiKOlqtMwgE_yGoX0_vOVHvEJs="
+                src="https://neurosciencenews.com/files/2023/06/coffee-brain-caffeine-neuroscincces.jpg"
                 className="d-block w-100"
                 alt="..."
               />
             </div>
             <div className="carousel-item">
               <img
-                src="https://media.istockphoto.com/id/1400194993/photo/cappuccino-art.webp?a=1&b=1&s=612x612&w=0&k=20&c=CN_BYkO9GKOH4JgMebiKOlqtMwgE_yGoX0_vOVHvEJs="
+                src="https://www.nescafe.com/mena/sites/default/files/2023-10/Coffee_Types_Banner_757x418.jpg"
                 className="d-block w-100"
                 alt="..."
               />
